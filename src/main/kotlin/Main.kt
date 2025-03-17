913a8f12530c42631b95a2e6c226868dbee105f4
Wendel Cutrim <wendelcutrim@hotmail.com>
2025-03-17 18:51:20 -0300

    Estudos sobre construtor primário e secundário no kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
 fun main() {
     var pessoa: Pessoa = Pessoa("Wendel", "1998");
+    pessoa.abc = "abc";
+    println(pessoa.acordar());
+    println(pessoa.abc);
 }
--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,7 +1,28 @@
 class Pessoa(var nome: String, var anoNascimento: String) {
     var abc: String? = null;
+    var doc: String? = null;
+
+    constructor(nome: String, anoNascimento: String, doc: String):  this(nome, anoNascimento) {
+        if (doc == "") {
+            throw IllegalArgumentException("Documento não informado!");
+        }
+
+        this.doc = doc;
+    }
 
     fun acordar(): String {
         return "Acorda ${this.nome}";
     }
 }
+
+// Classe que não pode ser instanciada.
+//class Pessoa2 private constructor() {}
+
+//fun main() {
+//    var pessoa: Pessoa = Pessoa("Wendel", "1998");
+//    pessoa.abc = "abc";
+//    println(pessoa.acordar());
+//    println(pessoa.abc);
+//
+////    var pessoa2 = Pessoa2()
+//}
