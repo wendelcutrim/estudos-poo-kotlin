09b1503a5964e88ff7e344d7848021c77cc8631f
Wendel Cutrim <wendelcutrim@hotmail.com>
2025-03-19 19:44:58 -0300

    Estudos classe anonima

--- src/main/kotlin/Polimorfismo.kt
+++ src/main/kotlin/Polimorfismo.kt
@@ -1,32 +1,15 @@
-abstract class Funcionario {
-    abstract fun calculaBonus(): Double
+interface Funcionario {
+    fun calculaBonus(): Double
 }
-
-class Gerente: Funcionario() {
-    override fun calculaBonus(): Double {
-        return 5000.0;
-    }
-}
-
-class Analista: Funcionario() {
-    override fun calculaBonus(): Double {
-        return 3000.0;
-    }
-}
-
-//fun imprimeValorBonos(gerente: Gerente) {
-//    println(gerente.calculaBonus());
-//}
-//
-//fun imprimeValorBonos(analista: Analista) {
-//    println(analista.calculaBonus());
-//}
 
 fun imprimeValorBonus(funcionario: Funcionario) {
     println(funcionario.calculaBonus());
 }
 
 fun main() {
-    imprimeValorBonus(Gerente());
-    imprimeValorBonus(Analista());
+    imprimeValorBonus(object : Funcionario {
+        override fun calculaBonus(): Double {
+            return 1000.0
+        }
+    });
 }
