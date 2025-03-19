21400739835778088a05e697b53d9488a0ecb1f8
Wendel Cutrim <wendelcutrim@hotmail.com>
2025-03-19 20:06:54 -0300

    Estudos lambda functions Kotlin

--- src/main/kotlin/Polimorfismo.kt
+++ src/main/kotlin/Polimorfismo.kt
@@ -1,15 +1,19 @@
-interface Funcionario {
-    fun calculaBonus(): Double
+
+// SAM - Single Abstract Method, ocorre quando a interface tem somente 1 método
+// Interface funcional. Coloca a palavra fun antes da palavra reservada interface
+// Lambda x classe anonima: É a mesma coisa, o que muda é que a lambda simplifca o código.
+fun interface Funcionario {
+    fun calculaBonus(nome: String, id: Int): Double
 }
 
 fun imprimeValorBonus(funcionario: Funcionario) {
-    println(funcionario.calculaBonus());
+    println(funcionario.calculaBonus("Test", 1));
 }
 
 fun main() {
-    imprimeValorBonus(object : Funcionario {
-        override fun calculaBonus(): Double {
-            return 1000.0
-        }
-    });
+    imprimeValorBonus { nome, id ->
+        val result = 10.0
+        println("Calculando o bônus de $id - $nome R$: $result");
+        result;
+    }
 }
